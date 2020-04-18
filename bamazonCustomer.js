ef4da9d5cce25563ecaf67a6a25a4be482af731e
Karina Titov <karinasul1995@gmail.com>
2020-04-18 09:38:49 -0500

    restored

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 
 var connection = mysql.createConnection({
@@ -13,7 +14,31 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-  });
+    showProducts();
+});
+
+
+function showProducts() {
+    console.log("All the items listed here");
+
+    var query = "select * from products";
+
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        var displayItems = new Table({
+            head: ["Item ID", "Product Name", "Department", "Price", "Quantity"],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayItems.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(displayItems.toString());
+        purchaseItem()
+    })
+
+}

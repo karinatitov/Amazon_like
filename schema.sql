d21827c2c8919f92276300b922a47d924e3d888c
Karina Titov <karinasul1995@gmail.com>
2020-05-11 16:31:12 -0500

    added schema and seeds

---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(255) NOT NULL,
+    department_name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    stock_quantity int NOT NULL,
+    PRIMARY KEY(item_id)
+    )
